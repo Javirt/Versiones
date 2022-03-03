233a32e09cef1ffae5cffbd4931548843b3fcc8e
javir <javir@DESKTOP-LFFF63D>
2022-03-03 17:43:30 +0100

    Primera versión del ejercicio de bucles

---
+++ src/controlversiones/ControlVersiones.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlversiones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author javir
+ */
+public class ControlVersiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);    
+        System.out.println("Ejercicio de Clase: Cuenta regresiva");    
+        System.out.println("Introduzca un número");
+        int num = sc.nextInt();
+        for(int i=num; i>=0; i--){
+            System.out.println(i);
+        }
+    }
+    
+}
